240f037cd113b4adb46a750821fd03a781922149
Enrico Marino <enrico.marino@email.com>
2013-06-05 18:13:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,90 +7,92 @@
  */
 
 /**
+ * Expose `classlist`
+ */
+
+module.exports = classlist;
+
+/**
  * classlist
- * 
- * @return {Element} this for chaining
- * @api public
  */
 
-exports.classlist = function () {
-  this.list = this.el.classList;
-};
+function classlist (element) {
+  
+  /**
+   * Utils
+   */
+   
+  var array = [];
+  var slice = array.slice;
+  
+  /**
+   * add_class
+   * Add class `name` if not already present.
+   *
+   * @param {String} name class name
+   * @return {Element} this for chaining
+   * @api public
+   */
+  
+  element.prototype.add_class = function (name) {
+    this.el.classList.add(name);
+    return this;
+  };
+  
+  /**
+   * remove_class
+   * Remove class `name` when present
+   *
+   * @param {String} name class to remove
+   * @return {Element} this for chaining
+   * @api public
+   */
+  
+  element.prototype.remove_class = function (name) {
+    this.el.classList.remove(name);
+    return this;
+  };
+  
+  /**
+   * toggle_class
+   * Toggle class `name`.
+   *
+   * @param {String} name class to toggle
+   * @return {ClassList} this for chaining
+   * @api public
+   */
+  
+  element.prototype.toggle_class = function (name) {
+    this.el.classList.toggle(name);
+    return this;
+  };
+  
+  /**
+   * classes
+   * Return an array of classes.
+   *
+   * @return {Array} array of classes
+   * @api public
+   */
+  
+  element.prototype.classes = function () {
+    return slice.call(this.el.classList);
+  };
+  
+  /**
+   * has_class|contain_class
+   * Check if class `name` is present.
+   *
+   * @param {String} name class name to check
+   * @return {Boolean} true if class `name` is present
+   *   false otherwise
+   * @api public
+   */
+  
+  element.prototype.has_class =
+  element.prototype.contains_class = function (name) {
+    return this.list.contains(name);
+  };
 
-/**
- * add_class
- * Add class `name` if not already present.
- *
- * @param {String} name class name
- * @return {Element} this for chaining
- * @api public
- */
-
-exports.add_class = function (name) {
-  this.list.add(name);
-  return this;
-};
-
-/**
- * remove_class
- * Remove class `name` when present
- *
- * @param {String} name class to remove
- * @return {Element} this for chaining
- * @api public
- */
-
-exports.remove_class = function (name) {
-  this.list.remove(name);
-  return this;
-};
-
-/**
- * toggle_class
- * Toggle class `name`.
- *
- * @param {String} name class to toggle
- * @return {ClassList} this for chaining
- * @api public
- */
-
-exports.toggle_class = function (name) {
-  this.list.toggle(name);
-  return this;
-};
-
-/**
- * class_array
- * Return an array of classes.
- *
- * @return {Array} array of classes
- * @api public
- */
-
-exports.class_array = function () {
-  var list = this.list;
-  var len = list.length;
-  var array = new Array(len);
-  var i;
-
-  for (i = 0; i < len; i += 1) {
-    array[i] = list[i];
-  }
-
-  return array;
-};
-
-/**
- * has_class|contain_class
- * Check if class `name` is present.
- *
- * @param {String} name class name to check
- * @return {Boolean} true if class `name` is present
- *   false otherwise
- * @api public
- */
-
-exports.has_class =
-exports.contains_class = function (name) {
-  return this.list.contains(name);
-};
+  return element;
+}
